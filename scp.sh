5f0b1cf9913c9ec570d90767cccbf57f13097c4c
logan-cote <70023849+logan-cote@users.noreply.github.com>
2023-07-28 23:24:39 -0400

    Update scp.sh

--- scp.sh
+++ scp.sh
@@ -3,12 +3,27 @@ hosts=(
 127.0.0.1
 10.0.2.15
 )
+usage="Usage: ./scp.sh local/file/location remote/file/location"
+if [ "$1" = "-h" ]; then
+	echo $usage;
+	exit
+elif [ "$1" = "" ]; then
+	echo $usage;
+	exit
+elif [ "$2" = "" ]; then
+	echo $usage;
+	exit
+fi
+
 username='kali'
-local_path='/home/kali/Desktop/list.txt'
-remote_path='/home/kali/'
+local_path=$1
+remote_path=$2
 
 for host in ${hosts[@]}
 do
 	scp -o StrictHostKeyChecking=no $local_path $username@$host:$remote_path;
-	ssh -o StrictHostKeyChecking=no $username@$host -t sudo rm -r $remote_path/list.txt
+	echo "";
+	ssh -o StrictHostKeyChecking=no $username@$host -t sudo rm -r $remote_path/list.txt;
+	echo"";
 done
+exit
